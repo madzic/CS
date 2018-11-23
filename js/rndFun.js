8a345080be9267ba33440a4cb97f9dee4523bd87
madzic <pajsernakvadrat@gmail.com>
2018-11-23 23:04:40 +0100

    Update rndFun.js

--- js/rndFun.js
+++ js/rndFun.js
@@ -1,7 +1,96 @@
 function genQuote() {
-  var randNum = Math.floor(Math.random()*8+1 );
-  document.getElementById('demo').innerHTML = quotes[randNum];
-}
+    
+    // document.getElementById('Sorted1a').innerHTML = document.getElementById('Igrac1a').value //quotes[randNum];
+    // document.getElementById('Sorted1a').innerHTML = document.getElementById('Igrac1a').innerHTML;
 
-//quote array
-var quotes = ["Quote 2", "Quote 3", "Quote 1"];
+// TIER 1
+
+    var randNum = Math.floor(Math.random()*99+1 );
+
+    if (randNum < 50) 
+        {
+        document.getElementById('Sorted1a').innerHTML = document.getElementById('Igrac1a').value;
+        document.getElementById('Sorted1b').innerHTML = document.getElementById('Igrac1b').value;
+        } 
+    else 
+        {
+        document.getElementById('Sorted1a').innerHTML = document.getElementById('Igrac1b').value;
+        document.getElementById('Sorted1b').innerHTML = document.getElementById('Igrac1a').value;
+        }
+
+// TIER 2
+
+    var randNum = Math.floor(Math.random()*99+1 );
+
+    if (randNum < 50) 
+        {
+        document.getElementById('Sorted2a').innerHTML = document.getElementById('Igrac2a').value;
+        document.getElementById('Sorted2b').innerHTML = document.getElementById('Igrac2b').value;
+        } 
+    else 
+        {
+        document.getElementById('Sorted2a').innerHTML = document.getElementById('Igrac2b').value;
+        document.getElementById('Sorted2b').innerHTML = document.getElementById('Igrac2a').value;
+        }
+
+// TIER 3
+
+var randNum = Math.floor(Math.random()*99+1 );
+
+if (randNum < 50) 
+    {
+    document.getElementById('Sorted3a').innerHTML = document.getElementById('Igrac3a').value;
+    document.getElementById('Sorted3b').innerHTML = document.getElementById('Igrac3b').value;
+    } 
+else 
+    {
+    document.getElementById('Sorted3a').innerHTML = document.getElementById('Igrac3b').value;
+    document.getElementById('Sorted3b').innerHTML = document.getElementById('Igrac3a').value;
+    }
+
+// TIER 4
+
+var randNum = Math.floor(Math.random()*99+1 );
+
+if (randNum < 50) 
+    {
+    document.getElementById('Sorted4a').innerHTML = document.getElementById('Igrac4a').value;
+    document.getElementById('Sorted4b').innerHTML = document.getElementById('Igrac4b').value;
+    } 
+else 
+    {
+    document.getElementById('Sorted4a').innerHTML = document.getElementById('Igrac4b').value;
+    document.getElementById('Sorted4b').innerHTML = document.getElementById('Igrac4a').value;
+    }
+
+// TIER 5
+
+var randNum = Math.floor(Math.random()*99+1 );
+
+if (randNum < 50) 
+    {
+    document.getElementById('Sorted5a').innerHTML = document.getElementById('Igrac5a').value;
+    document.getElementById('Sorted5b').innerHTML = document.getElementById('Igrac5b').value;
+    } 
+else 
+    {
+    document.getElementById('Sorted5a').innerHTML = document.getElementById('Igrac5b').value;
+    document.getElementById('Sorted5b').innerHTML = document.getElementById('Igrac5a').value;
+    }
+
+// TIER 6
+
+var randNum = Math.floor(Math.random()*99+1 );
+
+if (randNum < 50) 
+    {
+    document.getElementById('Sorted6a').innerHTML = document.getElementById('Igrac6a').value;
+    document.getElementById('Sorted6b').innerHTML = document.getElementById('Igrac6b').value;
+    } 
+else 
+    {
+    document.getElementById('Sorted6a').innerHTML = document.getElementById('Igrac6b').value;
+    document.getElementById('Sorted6b').innerHTML = document.getElementById('Igrac6a').value;
+    }
+
+}
